a48e59453939e9632da63931a6db71c29691b54e
jonitro165 <42643475+Jonitr0@users.noreply.github.com>
2022-07-22 22:12:26 +0200

    multiline comment matching working, keywords wip

--- utils/ndf_syntax_highlighter.py
+++ utils/ndf_syntax_highlighter.py
@@ -1,39 +1,59 @@
 from PySide2 import QtGui, QtCore
 from PySide2.QtCore import Qt
 
+
 # Based on: https://doc.qt.io/qt-6/qtwidgets-richtext-syntaxhighlighter-example.html
 
 class HighlightingRule:
     pattern = QtCore.QRegularExpression()
     format = QtGui.QTextCharFormat()
+    single_line_comment = False
+    name = ""
 
 
 class NdfSyntaxHighlighter(QtGui.QSyntaxHighlighter):
     highlighting_rules = []
 
-    multiline_comment_starts = ["{", "(*", "/*"]
-    multiline_comment_ends = ["}", "*)", "*/"]
+    # NDF Reference claims {} are comment delimiters, yet they are used in GUID
+    multiline_comment_starts = ["(*", "/*"]
+    multiline_comment_ends = ["*)", "*/"]
     multiline_comment_format = QtGui.QTextCharFormat()
+
+    keywords = ["export", "is", "template", "unnamed", "nil", "private", "int", "string", "true", "false", "div", "map"]
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        for keyword in self.keywords:
+            rule = HighlightingRule()
+            rule.pattern = QtCore.QRegularExpression("\\b" + keyword + "\\b",
+                                                     QtCore.QRegularExpression.CaseInsensitiveOption)
+            rule.format.setForeground(Qt.red)
+            rule.name = keyword
+            self.highlighting_rules.append(rule)
 
         single_comment_rule = HighlightingRule()
         single_comment_rule.format.setFontItalic(True)
         single_comment_rule.format.setForeground(Qt.lightGray)
         single_comment_rule.pattern = QtCore.QRegularExpression("//[^\n]*")
+        single_comment_rule.single_line_comment = True
         self.highlighting_rules.append(single_comment_rule)
 
         self.multiline_comment_format.setFontItalic(True)
         self.multiline_comment_format.setForeground(Qt.green)
 
     def highlightBlock(self, text: str):
+        # save potential single line comment start
+        single_line_comment_start = -1
         # apply basic rules
         for rule in self.highlighting_rules:
+            print(rule.name)
             iterator = rule.pattern.globalMatch(text)
             while iterator.hasNext():
                 match = iterator.next()
                 self.setFormat(match.capturedStart(), match.capturedLength(), rule.format)
+                if rule.single_line_comment:
+                    single_line_comment_start = match.capturedStart()
 
         # set block state to 0, "not in comment"
         self.setCurrentBlockState(0)
@@ -46,20 +66,25 @@ class NdfSyntaxHighlighter(QtGui.QSyntaxHighlighter):
                     start_index = max(text.index(start), start_index)
                 except ValueError:
                     pass
+            # ignore if it's in a single line comment
+            if start_index > -1 and start_index > single_line_comment_start >= 0:
+                start_index = -1
+        else:
+            start_index = 0
 
         while start_index >= 0:
+
             end_index = -1
             cap_size = 0
             for end in self.multiline_comment_ends:
                 try:
                     cap = text.index(end, start_index)
-                    end_index = max(cap, end_index)
                     if cap > end_index:
                         cap_size = len(end)
+                    end_index = max(cap, end_index)
                 except ValueError:
                     pass
-            comment_length = 0
-            # if no comment is found
+            # if no comment end is found
             if end_index == -1:
                 self.setCurrentBlockState(1)
                 comment_length = len(text) - start_index
@@ -72,6 +97,3 @@ class NdfSyntaxHighlighter(QtGui.QSyntaxHighlighter):
                     start_index = text.index(start, start_index + comment_length)
                 except ValueError:
                     pass
-
-
-
