303b3745a1bd13e68e1bd91d2162a28f5d66eb62
jonitro165 <42643475+Jonitr0@users.noreply.github.com>
2022-07-26 16:39:04 +0200

    shadow on mainwindow

--- main_window.py
+++ main_window.py
@@ -1,4 +1,5 @@
 from PySide2 import QtWidgets, QtCore
+from PySide2.QtCore import Qt
 
 from wme_widgets import wme_title_bar, main_widget
 from utils import warno_path_loader, settings_manager
@@ -7,6 +8,8 @@ from utils import warno_path_loader, settings_manager
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.shadow_layout = QtWidgets.QHBoxLayout()
+        self.shadow_effect = QtWidgets.QGraphicsDropShadowEffect()
         self.main_widget_ref = None
         QtCore.QTimer.singleShot(0, self.start_main_window)
 
@@ -19,19 +22,32 @@ class MainWindow(QtWidgets.QMainWindow):
         self.resize(1152, 648)
         self.setWindowTitle("WARNO Mod Editor")
 
+        shadow_widget = QtWidgets.QWidget()
+        self.shadow_layout.setMargin(4)
+        shadow_widget.setLayout(self.shadow_layout)
+
+        self.shadow_effect.setOffset(0, 0)
+        self.shadow_effect.setBlurRadius(4)
+        self.shadow_effect.setColor(Qt.black)
+        shadow_widget.setGraphicsEffect(self.shadow_effect)
+
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        shadow_widget.setAttribute(Qt.WA_TranslucentBackground)
+
         w = QtWidgets.QWidget()
         main_layout = QtWidgets.QVBoxLayout(self)
-        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setMargin(0)
         main_layout.setSpacing(0)
         w.setLayout(main_layout)
+        self.shadow_layout.addWidget(w)
 
         bar = wme_title_bar.WMETitleBar(parent=self)
         main_layout.addWidget(bar)
         self.main_widget_ref = main_widget.MainWidget(warno_path, bar)
         main_layout.addWidget(self.main_widget_ref)
 
-        self.setCentralWidget(w)
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setCentralWidget(shadow_widget)
+        self.setWindowFlags(Qt.FramelessWindowHint)
 
         self.grip = QtWidgets.QSizeGrip(self)
         self.grip.resize(16, 16)
@@ -41,10 +57,23 @@ class MainWindow(QtWidgets.QMainWindow):
     def resizeEvent(self, event):
         QtWidgets.QMainWindow.resizeEvent(self, event)
         rect = self.rect()
-        self.grip.move(rect.right() - 16, rect.bottom() - 16)
+        self.grip.move(rect.right() - 16 - 6, rect.bottom() - 16 - 6)
 
     def close(self):
         if self.main_widget_ref is None:
             super().close()
         if self.main_widget_ref.ask_all_tabs_to_save():
             super().close()
+
+    def changeEvent(self, event):
+        if event.type() == QtCore.QEvent.WindowStateChange:
+            if self.windowState() == Qt.WindowMaximized:
+                self.shadow_layout.setMargin(0)
+                self.shadow_effect.setEnabled(False)
+            else:
+                self.shadow_layout.setMargin(4)
+                # stupid but needed to fix shadow effect
+                self.resize(self.size().width() + 1, self.size().height() + 1)
+                self.resize(self.size().width() - 1, self.size().height() - 1)
+                self.shadow_effect.setEnabled(True)
+        super().changeEvent(event)
