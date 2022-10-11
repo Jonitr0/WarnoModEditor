607e1f90a5e4a7eac4d0542140f13f24e54ddd99
jonitro165 <42643475+Jonitr0@users.noreply.github.com>
2022-10-11 23:29:04 +0200

    filtering without proxy model

--- src/wme_widgets/wme_project_explorer.py
+++ src/wme_widgets/wme_project_explorer.py
@@ -1,4 +1,4 @@
-from PySide6 import QtWidgets, QtCore, QtGui
+from PySide6 import QtWidgets, QtCore
 from PySide6.QtCore import Qt
 
 from src.utils import icon_manager
@@ -14,23 +14,14 @@ class WMEProjectExplorer(QtWidgets.QWidget):
         self.setLayout(main_layout)
 
         # create minimalistic search bar
-        search_layout = QtWidgets.QHBoxLayout()
-        main_layout.addLayout(search_layout)
         search_bar = wme_lineedit.WMELineEdit()
         search_bar.setPlaceholderText("Find in Directory...")
-        search_layout.addWidget(search_bar)
-
-        search_icon = QtGui.QIcon()
-        search_icon.addPixmap(icon_manager.load_pixmap("magnify.png", COLORS.PRIMARY), QtGui.QIcon.Normal)
-        search_icon.addPixmap(icon_manager.load_pixmap("magnify.png", COLORS.SECONDARY_LIGHT), QtGui.QIcon.Disabled)
-        search_button = QtWidgets.QToolButton()
-        search_button.setIcon(search_icon)
-        search_button.setToolTip("Find Files")
-        search_button.setFixedSize(32, 32)
-        search_layout.addWidget(search_button)
+        main_layout.addWidget(search_bar)
 
         self.tree_view = FileSystemTreeView(self)
         main_layout.addWidget(self.tree_view)
+
+        search_bar.textChanged.connect(self.tree_view.on_find_text_changed)
 
 
 class FileSystemTreeView(QtWidgets.QTreeView):
@@ -43,22 +34,36 @@ class FileSystemTreeView(QtWidgets.QTreeView):
         self.setHeaderHidden(True)
         self.setSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Expanding)
         self.setMinimumWidth(160)
+        self.mod_path = ""
 
     def update_model(self, mod_path: str):
         data_model = QtWidgets.QFileSystemModel()
         data_model.setRootPath(mod_path)
         data_model.setNameFilters(["*.ndf"])
         data_model.setNameFilterDisables(False)
+        data_model.setFilter(QtCore.QDir.Files | QtCore.QDir.Dirs)
         data_model.setIconProvider(FileIconProvider())
 
+        #proxy_model = QtCore.QSortFilterProxyModel()
+        #proxy_model.setSourceModel(data_model)
+        #proxy_model.setRecursiveFilteringEnabled(True)
+        # TODO: make this filter stuff
+
+        #self.setModel(proxy_model)
         self.setModel(data_model)
+        #root_index = proxy_model.mapFromSource(data_model.index(mod_path))
+        #self.setRootIndex(root_index)
         self.setRootIndex(data_model.index(mod_path))
+        self.mod_path = mod_path
 
         self.hideColumn(1)
         self.hideColumn(2)
         self.hideColumn(3)
 
     def on_double_click(self, index):
+        # map index to source
+        index = self.model().mapToSource(index)
+        #file_path = QtWidgets.QFileSystemModel.filePath(self.model().sourceModel(), index)
         file_path = QtWidgets.QFileSystemModel.filePath(self.model(), index)
         if file_path.endswith(".ndf"):
             self.open_ndf_editor.emit(file_path)
@@ -66,6 +71,18 @@ class FileSystemTreeView(QtWidgets.QTreeView):
     def mouseDoubleClickEvent(self, event):
         if event.button() == Qt.LeftButton:
             super().mouseDoubleClickEvent(event)
+
+    def on_find_text_changed(self, text: str):
+        if text == "":
+            self.model().setNameFilters(["*.ndf"])
+            self.collapseAll()
+        else:
+            text = text.replace(".", "\\.")
+            self.model().setNameFilters(["*" + text + "*.ndf"])
+            self.expandRecursively(self.model().index(self.mod_path))
+
+        #reg_exp = QtCore.QRegularExpression(text, QtCore.QRegularExpression.CaseInsensitiveOption)
+        #self.model().setFilterRegularExpression(reg_exp)
 
 
 class FileIconProvider(QtWidgets.QFileIconProvider):
