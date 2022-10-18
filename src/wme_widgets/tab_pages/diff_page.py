ecdfdc011a32f7b63a1d15ef649b8e1f4c357bbd
jonitro165 <42643475+Jonitr0@users.noreply.github.com>
2022-10-18 18:08:38 +0200

    left/right only basics working

--- src/wme_widgets/tab_pages/diff_page.py
+++ src/wme_widgets/tab_pages/diff_page.py
@@ -15,6 +15,7 @@ import random
 class DiffPage(tab_page_base.TabPageBase):
     def __init__(self):
         super().__init__()
+        self.results_layout = QtWidgets.QVBoxLayout()
         self.compare_button = QtWidgets.QPushButton("Compare")
         self.target_combobox = QtWidgets.QComboBox()
         self.setup_ui()
@@ -40,10 +41,15 @@ class DiffPage(tab_page_base.TabPageBase):
         target_selection_layout.addWidget(self.compare_button)
         target_selection_layout.addStretch(0)
 
-        results_area = QtWidgets.QScrollArea()
+        # TODO: check if this is shown
+        results_area = QtWidgets.QScrollArea(self)
+        results_area.setWidgetResizable(True)
         main_layout.addWidget(results_area)
-        results_list_widget = QtWidgets.QWidget()
+        results_list_widget = QtWidgets.QWidget(self)
         results_area.setWidget(results_list_widget)
+        results_list_widget.setLayout(self.results_layout)
+
+        self.results_layout.addStretch(1)
 
     def load_mods_to_combobox(self):
         # get Mods dir
@@ -61,6 +67,13 @@ class DiffPage(tab_page_base.TabPageBase):
                 self.target_combobox.addItem(next_name, next)
 
     def on_compare(self):
+        # clear results layout
+        for i in reversed(range(self.results_layout.count())):
+            widget_to_remove = self.results_layout.itemAt(i).widget()
+            self.results_layout.removeWidget(widget_to_remove)
+            if widget_to_remove:
+                widget_to_remove.setParent(None)
+
         main_widget.MainWidget.instance.show_loading_screen("Running comparison...")
 
         target = self.target_combobox.currentData()
@@ -79,24 +92,84 @@ class DiffPage(tab_page_base.TabPageBase):
         res = dircmp(mod_dir, target)
         res_d, res_l, res_r = self.compare_subdirs(res, [], [], [])
         # TODO: process results
+
+        left_name = main_widget.MainWidget.instance.get_loaded_mod_name()
+        if delete:
+            right_name = "unmodded game files"
+        else:
+            right_name = target[target.rindex('/') + 1:]
+
         print(res_d)
         print(res_l)
         print(res_r)
 
+        for diff in res_l:
+            diff_w = DiffWidget(self)
+            diff_w.left_only(diff, left_name)
+            self.results_layout.insertWidget(self.results_layout.count()-1, diff_w)
+
+        for diff in res_r:
+            diff_w = DiffWidget(self)
+            diff_w.right_only(diff, right_name)
+            self.results_layout.insertWidget(self.results_layout.count()-1, diff_w)
+
         if delete:
             shutil.rmtree(target)
 
+        if len(res_d) < 1 and len(res_l) < 1 and len(res_r) < 1:
+            self.results_layout.insertWidget(self.results_layout.count()-1, QtWidgets.QLabel("No differences found."))
+
         main_widget.MainWidget.instance.hide_loading_screen()
 
-    def compare_subdirs(self, dcmp: dircmp, diffs: list[str], left: list[str], right: list[str]):
-        # TODO: add full path
-        diffs += dcmp.diff_files
-        left += dcmp.left_only
-        right += dcmp.right_only
-        for sub_dcmp in dcmp.subdirs.values():
-            sub_diffs, sub_left, sub_right = self.compare_subdirs(sub_dcmp, [], [], [])
+    def compare_subdirs(self, dcmp: dircmp, diffs: list[str], left: list[str], right: list[str], path=""):
+        # add full path to diffs and left/right
+        diffs_new = dcmp.diff_files
+        diffs_new = [path + i for i in diffs_new]
+        diffs += diffs_new
+
+        left_new = dcmp.left_only
+        left_new = [path + i for i in left_new]
+        left += left_new
+
+        right_new = dcmp.right_only
+        right_new = [path + i for i in right_new]
+        right += right_new
+
+        for subdir in dcmp.subdirs:
+            sub_diffs, sub_left, sub_right = self.compare_subdirs(dcmp.subdirs[subdir], [], [], [], path + subdir + "/")
             diffs += sub_diffs
             left += sub_left
             right += sub_right
+
         return diffs, left, right
 
+
+class DiffWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.text_edit = QtWidgets.QTextEdit()
+        self.open_in_editor_button = QtWidgets.QPushButton("Open in text editor")
+        self.info_label = QtWidgets.QLabel()
+        self.setup_ui()
+
+    def setup_ui(self):
+        main_layout = QtWidgets.QVBoxLayout()
+        self.setLayout(main_layout)
+
+        info_layout = QtWidgets.QHBoxLayout()
+        main_layout.addLayout(info_layout)
+
+        info_layout.addWidget(self.info_label)
+        info_layout.addStretch(1)
+        info_layout.addWidget(self.open_in_editor_button)
+
+        main_layout.addWidget(self.text_edit)
+
+    def left_only(self, diff_name: str, left_name: str):
+        self.info_label.setText(diff_name + " only exists in " + left_name)
+        self.text_edit.setHidden(True)
+
+    def right_only(self, diff_name: str, right_name: str):
+        self.info_label.setText(diff_name + " only exists in " + right_name)
+        self.text_edit.setHidden(True)
