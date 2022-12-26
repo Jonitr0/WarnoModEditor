806205a172ae853d3013454e954ad6af9560db7d
Jonitr0 <42643475+Jonitr0@users.noreply.github.com>
2022-12-27 00:09:40 +0100

    diff code editor WIP

--- src/wme_widgets/tab_pages/diff_page/diff_widget.py
+++ src/wme_widgets/tab_pages/diff_page/diff_widget.py
@@ -1,5 +1,7 @@
 from PySide6 import QtWidgets
 
+from src.wme_widgets.tab_pages.diff_page import diff_code_editor
+
 
 # TODO: add colored icon for faster readability
 class DiffWidget(QtWidgets.QFrame):
@@ -10,11 +12,14 @@ class DiffWidget(QtWidgets.QFrame):
         self.buffer_lines = 1
         # max. number of identical lines between two diff blocks
         self.max_space_between_diff_blocks = 3
+        # max. number of adjacent changed lines until break
+        self.max_diff_display_length = 10
 
         # TODO: make this a layout to list multiple potential diffs
-        self.text_edit = QtWidgets.QTextEdit()
         self.open_in_editor_button = QtWidgets.QPushButton("Open in text editor")
         self.info_label = QtWidgets.QLabel()
+        self.diff_layout = QtWidgets.QVBoxLayout()
+
         self.setObjectName("list_entry")
         self.setup_ui()
 
@@ -29,19 +34,22 @@ class DiffWidget(QtWidgets.QFrame):
         info_layout.addStretch(1)
         info_layout.addWidget(self.open_in_editor_button)
 
-        main_layout.addWidget(self.text_edit)
+        main_layout.addLayout(self.diff_layout)
 
     def left_only(self, diff_name: str, left_name: str):
         self.info_label.setText(diff_name + " only exists in " + left_name)
-        self.text_edit.setHidden(True)
+        # TODO: only show button if file
 
     def right_only(self, diff_name: str, right_name: str):
         self.info_label.setText(diff_name + " only exists in " + right_name)
         self.open_in_editor_button.setHidden(True)
-        self.text_edit.setHidden(True)
 
     def changed_text_file(self, diff_name: str, changed_lines: list, left_lines: list, right_lines: list):
         self.info_label.setText(diff_name + " contains " + str(len(changed_lines)) + " changed line(s):")
         # TODO: create diff blocks
         # TODO: add text edit per diff block
         # TODO: add open in editor button per diff block
+        for line in changed_lines:
+            text_edit = diff_code_editor.DiffCodeEditor()
+            text_edit.appendPlainText(left_lines[line])
+            self.diff_layout.addWidget(text_edit)
