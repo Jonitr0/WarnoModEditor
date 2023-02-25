87fb03639258238036ef32fccf2aff0ec8bf4bbf
jonitro165 <42643475+Jonitr0@users.noreply.github.com>
2023-02-25 22:39:56 +0100

    modern resizing (thx stackoverflow)

--- src/wme_widgets/base_window.py
+++ src/wme_widgets/base_window.py
@@ -5,10 +5,69 @@ from PySide6.QtCore import Qt
 from src.wme_widgets import wme_title_bar
 from src.utils import icon_manager
 
+# Side Grips based on:
+# https://stackoverflow.com/questions/62807295/how-to-resize-a-window-from-the-edges-after-adding-the-property-qtcore-qt-framel
+
+class SideGrip(QtWidgets.QWidget):
+    def __init__(self, parent, edge):
+        super().__init__(parent)
+        if edge == Qt.LeftEdge:
+            self.setCursor(Qt.SizeHorCursor)
+            self.resizeFunc = self.resizeLeft
+        elif edge == Qt.TopEdge:
+            self.setCursor(Qt.SizeVerCursor)
+            self.resizeFunc = self.resizeTop
+        elif edge == Qt.RightEdge:
+            self.setCursor(Qt.SizeHorCursor)
+            self.resizeFunc = self.resizeRight
+        else:
+            self.setCursor(Qt.SizeVerCursor)
+            self.resizeFunc = self.resizeBottom
+        self.mousePos = None
+
+    def resizeLeft(self, delta):
+        window = self.window()
+        width = max(window.minimumWidth(), window.width() - delta.x())
+        geo = window.geometry()
+        geo.setLeft(geo.right() - width)
+        window.setGeometry(geo)
+
+    def resizeTop(self, delta):
+        window = self.window()
+        height = max(window.minimumHeight(), window.height() - delta.y())
+        geo = window.geometry()
+        geo.setTop(geo.bottom() - height)
+        window.setGeometry(geo)
+
+    def resizeRight(self, delta):
+        window = self.window()
+        width = max(window.minimumWidth(), window.width() + delta.x())
+        window.resize(width, window.height())
+
+    def resizeBottom(self, delta):
+        window = self.window()
+        height = max(window.minimumHeight(), window.height() + delta.y())
+        window.resize(window.width(), height)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.mousePos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if self.mousePos is not None:
+            delta = event.pos() - self.mousePos
+            self.resizeFunc(delta)
+
+    def mouseReleaseEvent(self, event):
+        self.mousePos = None
+
+
 SHADOW_WIDTH = 8
 
 
 class BaseWindow(QtWidgets.QWidget):
+    _gripSize = SHADOW_WIDTH
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -16,7 +75,7 @@ class BaseWindow(QtWidgets.QWidget):
         self.shadow_effect = QtWidgets.QGraphicsDropShadowEffect()
 
         shadow_widget = QtWidgets.QWidget()
-        self.shadow_layout.setContentsMargins(SHADOW_WIDTH, SHADOW_WIDTH, SHADOW_WIDTH, SHADOW_WIDTH)
+        self.shadow_layout.setContentsMargins(0, 0, 0, 0)
         shadow_widget.setLayout(self.shadow_layout)
 
         self.shadow_effect.setOffset(0, 0)
@@ -25,7 +84,6 @@ class BaseWindow(QtWidgets.QWidget):
         shadow_widget.setGraphicsEffect(self.shadow_effect)
 
         self.setAttribute(Qt.WA_TranslucentBackground)
-        shadow_widget.setAttribute(Qt.WA_TranslucentBackground)
         self.setWindowFlags(Qt.FramelessWindowHint)
 
         outer_layout = QtWidgets.QHBoxLayout()
@@ -44,26 +102,83 @@ class BaseWindow(QtWidgets.QWidget):
         self.title_bar = wme_title_bar.WMETitleBar(self)
         self.bar_layout.addWidget(self.title_bar)
 
-        self.grip = QtWidgets.QSizeGrip(self)
-        self.grip.resize(16, 16)
-
         self.setWindowIcon(QtGui.QIcon(icon_manager.load_colored_icon("app_icon_colored")))
+
+        self.sideGrips = [
+            SideGrip(self, QtCore.Qt.LeftEdge),
+            SideGrip(self, QtCore.Qt.TopEdge),
+            SideGrip(self, QtCore.Qt.RightEdge),
+            SideGrip(self, QtCore.Qt.BottomEdge),
+        ]
+
+        self.cornerGrips = [QtWidgets.QSizeGrip(self) for i in range(4)]
 
     def resizeEvent(self, event):
         super().resizeEvent(event)
-        rect = self.rect()
-        self.grip.move(rect.right() - 16 - SHADOW_WIDTH, rect.bottom() - 16 - SHADOW_WIDTH)
-        # TODO (0.1.1): add borders that allow resize, in shadow area
+        self.updateGrips()
 
     def changeEvent(self, event):
         if event.type() == QtCore.QEvent.WindowStateChange:
             if (self.windowState() == (Qt.WindowMaximized or Qt.WindowFullScreen)) or int(self.windowState()) == 6:
-                self.shadow_layout.setContentsMargins(0, 0, 0, 0)
                 self.shadow_effect.setEnabled(False)
+                self.set_grips_hidden(True)
             else:
-                self.shadow_layout.setContentsMargins(SHADOW_WIDTH, SHADOW_WIDTH, SHADOW_WIDTH, SHADOW_WIDTH)
-                # stupid but needed to fix shadow effect
-                self.resize(self.size().width() + 1, self.size().height() + 1)
-                self.resize(self.size().width() - 1, self.size().height() - 1)
                 self.shadow_effect.setEnabled(True)
+                self.set_grips_hidden(False)
         super().changeEvent(event)
+
+    def set_grips_hidden(self, hidden: bool):
+        for grip in self.sideGrips:
+            grip.setHidden(hidden)
+        for grip in self.cornerGrips:
+            grip.setHidden(hidden)
+
+    @property
+    def gripSize(self):
+        return self._gripSize
+
+    def setGripSize(self, size):
+        if size == self._gripSize:
+            return
+        self._gripSize = max(2, size)
+        self.updateGrips()
+
+    def updateGrips(self):
+        if self.windowState() == (Qt.WindowMaximized or Qt.WindowFullScreen):
+            self.setContentsMargins(*[0] * 4)
+            return
+
+        self.setContentsMargins(*[self.gripSize] * 4)
+
+        outRect = self.rect()
+        # an "inner" rect used for reference to set the geometries of size grips
+        inRect = outRect.adjusted(self.gripSize, self.gripSize,
+                                  -self.gripSize, -self.gripSize)
+
+        # top left
+        self.cornerGrips[0].setGeometry(
+            QtCore.QRect(outRect.topLeft(), inRect.topLeft()))
+        # top right
+        self.cornerGrips[1].setGeometry(
+            QtCore.QRect(outRect.topRight(), inRect.topRight()).normalized())
+        # bottom right
+        self.cornerGrips[2].setGeometry(
+            QtCore.QRect(inRect.bottomRight(), outRect.bottomRight()))
+        # bottom left
+        self.cornerGrips[3].setGeometry(
+            QtCore.QRect(outRect.bottomLeft(), inRect.bottomLeft()).normalized())
+
+        # left edge
+        self.sideGrips[0].setGeometry(
+            0, inRect.top(), self.gripSize, inRect.height())
+        # top edge
+        self.sideGrips[1].setGeometry(
+            inRect.left(), 0, inRect.width(), self.gripSize)
+        # right edge
+        self.sideGrips[2].setGeometry(
+            inRect.left() + inRect.width(),
+            inRect.top(), self.gripSize, inRect.height())
+        # bottom edge
+        self.sideGrips[3].setGeometry(
+            self.gripSize, inRect.top() + inRect.height(),
+            inRect.width(), self.gripSize)
