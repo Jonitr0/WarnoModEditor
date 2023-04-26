5f543ba21d37bcdbbc260302baab249c010e491b
jonitro165 <42643475+Jonitr0@users.noreply.github.com>
2023-04-26 17:57:11 +0200

    points loaded in settings page

--- src/wme_widgets/tab_pages/napo_pages/game_settings_page.py
+++ src/wme_widgets/tab_pages/napo_pages/game_settings_page.py
@@ -9,24 +9,35 @@ from src.wme_widgets.tab_pages.napo_pages import base_napo_page
 class GameSettingsPage(base_napo_page.BaseNapoPage):
     def __init__(self):
         super().__init__()
+        self.constants_napo = None
+        self.starting_points = []
 
         main_widget.MainWidget.instance.show_loading_screen("loading GDConstantes.ndf...")
 
-        file_path = os.path.join(main_widget.MainWidget.instance.get_loaded_mod_path(),
-                                 "GameData\\Gameplay\\Constantes\\GDConstantes.ndf")
-        self.constants_napo = self.get_napo_from_file(file_path)
-
         self.setup_ui()
+        self.update_page()
 
         main_widget.MainWidget.instance.hide_loading_screen()
 
     def setup_ui(self):
         main_layout = QtWidgets.QVBoxLayout()
-        main_layout.addWidget(QtWidgets.QLabel("Edit game settings such as starting points, income and victory conditions."))
+        main_layout.addWidget(
+            QtWidgets.QLabel("Edit game settings such as starting points, income and victory conditions."))
 
-        list_widget = QtWidgets.QListWidget()
-        #list_widget.addItems()
-        main_layout.addWidget(list_widget)
+        # TODO: create own list widget with: title, add button, input field, remove button
+        self.starting_pts_list_widget = QtWidgets.QListWidget()
+        main_layout.addWidget(self.starting_pts_list_widget)
 
         main_layout.addStretch(1)
         self.setLayout(main_layout)
+
+    def update_page(self):
+        file_path = os.path.join(main_widget.MainWidget.instance.get_loaded_mod_path(),
+                                 "GameData\\Gameplay\\Constantes\\GDConstantes.ndf")
+
+        self.constants_napo = self.get_napo_from_file(file_path)
+        self.starting_points = self.constants_napo.find("WargameConstantes\\ConquestPossibleScores")
+
+        self.starting_pts_list_widget.clear()
+        str_starting_points = [str(i) for i in self.starting_points]
+        self.starting_pts_list_widget.addItems(str_starting_points)
