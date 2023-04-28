24ddeebd40fb80610253b9b4e22d356500696835
Jonitr0 <42643475+Jonitr0@users.noreply.github.com>
2023-04-28 11:48:40 +0200

    indent on generated ndf

--- src/ndf_parser/ndf_converter/napo_to_ndf_converter.py
+++ src/ndf_parser/ndf_converter/napo_to_ndf_converter.py
@@ -2,8 +2,20 @@ from src.ndf_parser.napo_entities.napo_collection import *
 from src.ndf_parser.napo_entities.napo_assignment import *
 
 
+INDENT_SIZE = 4
+
 class NapoToNdfConverter:
-    # TODO: formatting: add variable indent
+    def __init__(self):
+        self.indent = 0
+
+    def increase_indent(self):
+        self.indent += INDENT_SIZE
+
+    def decrease_indent(self):
+        self.indent -= INDENT_SIZE
+        self.indent = max(0, self.indent)
+
+    # TODO: formatting: add break up of structures based on line length
     def convert(self, napo_file: NapoFile) -> str:
         return self.convert_entity(napo_file)
 
@@ -41,13 +53,16 @@ class NapoToNdfConverter:
                 result_str = ""
                 if entity.obj_type != "":
                     result_str = entity.obj_type + "\n"
-                result_str += "(\n"
+                result_str += (self.indent - INDENT_SIZE) * " " + "(\n"
                 for i in range(len(entity.value)):
                     result_str += self.convert_entity(entity.value[i])
-                result_str += "\n)"
+                result_str += (self.indent - INDENT_SIZE) * " " + ")"
             case NapoDatatype.STRUCTURAL:
                 if type(entity) == NapoAssignment:
                     result_str = ""
+                    if type(entity.value) == NapoObject:
+                        result_str += "\n"
+                    result_str += self.indent * " "
                     if entity.export:
                         result_str = "export "
                     if entity.member:
@@ -55,7 +70,9 @@ class NapoToNdfConverter:
                     else:
                         result_str += entity.id + " is "
                     val_entity = entity.value
+                    self.increase_indent()
                     result_str += self.convert_entity(val_entity) + "\n"
+                    self.decrease_indent()
                 elif type(entity) == NapoFile:
                     result_str = ""
                     for val in entity.value:
