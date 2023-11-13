4952301aafbf49dd9f5f38f0093c90307d494c05
Jonitr0 <42643475+Jonitr0@users.noreply.github.com>
2023-11-13 15:31:23 +0100

    Update edit_mod_config_dialog.py

--- src/dialogs/edit_mod_config_dialog.py
+++ src/dialogs/edit_mod_config_dialog.py
@@ -8,6 +8,10 @@ from src.wme_widgets import wme_essentials, main_widget, steam_text_edit
 
 class EditModConfigDialog(BaseDialog):
     def __init__(self, config_values: dict):
+        self.deck_format_version_spinbox = wme_essentials.WMESpinbox()
+        self.mod_version_spinbox = wme_essentials.WMESpinbox()
+        self.cosmetic_checkbox = QtWidgets.QCheckBox()
+        self.name_line_edit = wme_essentials.WMELineEdit()
         self.warning_label = QtWidgets.QLabel("WARNING! Uploading this mod might fail if it's name does not match "
                                               "the name of the directory (" +
                                               main_widget.instance.get_loaded_mod_name() + ")")
@@ -24,29 +28,25 @@ class EditModConfigDialog(BaseDialog):
         form_layout = QtWidgets.QFormLayout(self)
         self.main_layout.addLayout(form_layout)
 
-        name_line_edit = wme_essentials.WMELineEdit()
-        name_line_edit.setText(str(self.config_values["Properties/Name"]))
-        name_line_edit.textChanged.connect(self.on_name_changed)
+        self.name_line_edit.setText(str(self.config_values["Properties/Name"]))
+        self.name_line_edit.textChanged.connect(self.on_name_changed)
         name_label = QtWidgets.QLabel("Name")
         name_label.setToolTip("The name of the mod as it will be displayed in-game.")
-        form_layout.addRow(name_label, name_line_edit)
+        form_layout.addRow(name_label, self.name_line_edit)
 
-        if not bool(name_line_edit.text()):
+        if not bool(self.name_line_edit.text()):
             self.warning_label.setHidden(True)
         else:
-            self.warning_label.setHidden(name_line_edit.text() == main_widget.instance.get_loaded_mod_name())
+            self.warning_label.setHidden(self.name_line_edit.text() == main_widget.instance.get_loaded_mod_name())
         self.warning_label.setWordWrap(True)
         form_layout.addWidget(self.warning_label)
 
-        # TODO: make this a proper markdown textedit
         self.description_text_edit.set_text(str(self.config_values["Properties/Description"]))
-        self.description_text_edit.text_edit.textChanged.connect(self.on_description_changed)
         description_label = QtWidgets.QLabel("Description")
         description_label.setToolTip("A short description of the mod that will be displayed on Steam Workshop.")
         form_layout.addRow(description_label, self.description_text_edit)
 
         self.icon_path_line_edit.setText(str(self.config_values["Properties/PreviewImagePath"]))
-        self.icon_path_line_edit.textChanged.connect(self.on_icon_path_changed)
         browse_button = QtWidgets.QPushButton()
         browse_button.setText("Browse..")
         browse_button.clicked.connect(self.on_icon_browse)
@@ -57,43 +57,30 @@ class EditModConfigDialog(BaseDialog):
         icon_path_label.setToolTip("Path to a file which will be the preview image of the mod on Steam Workshop.")
         form_layout.addRow(icon_path_label, icon_path_layout)
 
-        cosmetic_checkbox = QtWidgets.QCheckBox()
-        cosmetic_checkbox.setChecked(int(self.config_values["Properties/CosmeticOnly"]))
-        cosmetic_checkbox.stateChanged.connect(self.on_cosmetic_changed)
+        self.cosmetic_checkbox.setChecked(int(self.config_values["Properties/CosmeticOnly"]))
         cosmetic_label = QtWidgets.QLabel("Cosmetic only")
         cosmetic_label.setToolTip("Check this box if the mod does not affect gameplay.")
-        form_layout.addRow(cosmetic_label, cosmetic_checkbox)
+        form_layout.addRow(cosmetic_label, self.cosmetic_checkbox)
 
-        mod_version_spinbox = wme_essentials.WMESpinbox()
-        mod_version_spinbox.setMinimum(0)
-        mod_version_spinbox.setValue(int(self.config_values["Properties/Version"]))
-        mod_version_spinbox.valueChanged.connect(self.on_mod_version_changed)
+        self.mod_version_spinbox.setMinimum(0)
+        self.mod_version_spinbox.setValue(int(self.config_values["Properties/Version"]))
         mod_version_label = QtWidgets.QLabel("Mod Version")
         mod_version_label.setToolTip("Increment this value to invalidate when an update of the mod is "
                                      "no longer compatible with older versions.")
-        form_layout.addRow(mod_version_label, mod_version_spinbox)
+        form_layout.addRow(mod_version_label, self.mod_version_spinbox)
 
-        deck_format_version_spinbox = wme_essentials.WMESpinbox()
-        deck_format_version_spinbox.setMinimum(0)
-        deck_format_version_spinbox.setValue(int(self.config_values["Properties/DeckFormatVersion"]))
-        deck_format_version_spinbox.valueChanged.connect(self.on_deck_format_version_changed)
+        self.deck_format_version_spinbox.setMinimum(0)
+        self.deck_format_version_spinbox.setValue(int(self.config_values["Properties/DeckFormatVersion"]))
         deck_format_version_label = QtWidgets.QLabel("Deck Format Version")
         deck_format_version_label.setToolTip("Set this at least to 1 if the mod affects gameplay.\n"
                                              "Increment this value to invalidate decks of older versions of the mod.")
-        form_layout.addRow(deck_format_version_label, deck_format_version_spinbox)
+        form_layout.addRow(deck_format_version_label, self.deck_format_version_spinbox)
 
     def on_name_changed(self, name: str):
-        self.config_values["Properties/Name"] = name
         if not bool(name):
             self.warning_label.setHidden(True)
         else:
             self.warning_label.setHidden(name == main_widget.instance.get_loaded_mod_name())
-
-    def on_description_changed(self):
-        self.config_values["Properties/Description"] = self.description_text_edit.get_text()
-
-    def on_icon_path_changed(self, icon_path: str):
-        self.config_values["Properties/PreviewImagePath"] = icon_path
 
     def on_icon_browse(self):
         icon_path = self.icon_path_line_edit.text()
@@ -103,20 +90,17 @@ class EditModConfigDialog(BaseDialog):
                                                                options=QtWidgets.QFileDialog.ReadOnly)
         self.icon_path_line_edit.setText(icon_path)
 
-    def on_cosmetic_changed(self, state):
-        self.config_values["Properties/CosmeticOnly"] = state
-
-    def on_mod_version_changed(self, version):
-        self.config_values["Properties/Version"] = version
-
-    def on_deck_format_version_changed(self, version):
-        self.config_values["Properties/DeckFormatVersion"] = version
-
     def get_config_values(self):
+        self.config_values["Properties/Name"] = self.name_line_edit.text()
+        self.config_values["Properties/Description"] = self.description_text_edit.get_text()
+        self.config_values["Properties/PreviewImagePath"] = self.icon_path_line_edit.text()
+        self.config_values["Properties/CosmeticOnly"] = self.cosmetic_checkbox.isChecked()
+        self.config_values["Properties/Version"] = self.mod_version_spinbox.value()
+        self.config_values["Properties/DeckFormatVersion"] = self.deck_format_version_spinbox.value()
         return self.config_values
 
     def reject(self) -> None:
-        if self.config_values != self.orig_config_values:
+        if self.get_config_values() != self.orig_config_values:
             dialog = essential_dialogs.ConfirmationDialog("All changes will be lost. Do you want to continue?",
                                                           "Warning!")
             if not dialog.exec_():
