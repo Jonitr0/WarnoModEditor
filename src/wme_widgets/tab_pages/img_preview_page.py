598bf795c417c307342c563bfdd95794c2a8985d
Jonitr0 <42643475+Jonitr0@users.noreply.github.com>
2024-12-30 22:55:30 +0100

    Image Preview Page working

--- src/wme_widgets/tab_pages/img_preview_page.py
+++ src/wme_widgets/tab_pages/img_preview_page.py
@@ -16,6 +16,8 @@ class ImgPreviewPage(base_tab_page.BaseTabPage):
         super().__init__()
         self.img_path = ""
         self.img_size = QtCore.QSize()
+        self.saved_size = QtCore.QSize()
+        self.original_aspect_ratio = 1.0
         self.setup_ui()
 
     def setup_ui(self):
@@ -40,13 +42,16 @@ class ImgPreviewPage(base_tab_page.BaseTabPage):
 
         resize_layout.addWidget(QtWidgets.QLabel("Width:"))
         self.width_spin_box.setRange(1, 9999)
+        self.width_spin_box.valueChanged.connect(self.on_width_changed)
         resize_layout.addWidget(self.width_spin_box)
 
         resize_layout.addWidget(QtWidgets.QLabel("Height:"))
         self.height_spin_box.setRange(1, 9999)
+        self.height_spin_box.valueChanged.connect(self.on_height_changed)
         resize_layout.addWidget(self.height_spin_box)
 
-        resize_layout.addWidget(QtWidgets.QLabel("Keep Aspect Ratio:"))
+        resize_layout.addWidget(QtWidgets.QLabel("Keep Original Aspect Ratio:"))
+        self.ratio_check_box.setChecked(True)
         resize_layout.addWidget(self.ratio_check_box)
 
         resize_widget = QtWidgets.QWidget()
@@ -54,25 +59,68 @@ class ImgPreviewPage(base_tab_page.BaseTabPage):
         tool_bar.addWidget(resize_widget)
 
         main_layout.addWidget(tool_bar)
-        main_layout.addStretch(1)
 
         self.img_preview.setAlignment(QtCore.Qt.AlignCenter)
         self.img_preview.setMinimumSize(1, 1)
-        self.img_preview.setSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        self.img_preview.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         main_layout.addWidget(self.img_preview)
-        main_layout.addStretch(1)
 
         self.help_file_path = "DontShow"
 
     def on_save(self):
         pixmap = self.img_preview.pixmap()
         pixmap.save(self.img_path)
+        self.saved_size = self.img_size
 
     def on_restore(self):
         self.set_image(self.img_path)
 
+    def on_width_changed(self, value: int):
+        if self.ratio_check_box.isChecked():
+            h = int(value / self.original_aspect_ratio)
+            self.height_spin_box.blockSignals(True)
+            self.height_spin_box.setValue(h)
+            self.height_spin_box.blockSignals(False)
+            self.img_size.setHeight(h)
+            w = min(value, self.img_preview.width())
+            h = min(h, self.img_preview.height())
+            self.img_preview.setPixmap(QtGui.QPixmap(self.img_path).scaled(w, h, QtCore.Qt.KeepAspectRatio))
+        else:
+            w = min(value, self.img_preview.width())
+            h = min(self.img_size.width(), self.img_preview.height())
+            self.img_preview.setPixmap(QtGui.QPixmap(self.img_path).scaled(w, h))
+        self.img_size.setWidth(value)
+        self.unsaved_changes = self.img_size == self.saved_size
+
+    def on_height_changed(self, value: int):
+        if self.ratio_check_box.isChecked():
+            w = int(value * self.original_aspect_ratio)
+            self.width_spin_box.blockSignals(True)
+            self.width_spin_box.setValue(w)
+            self.width_spin_box.blockSignals(False)
+            self.img_size.setWidth(w)
+            w = min(w, self.img_preview.width())
+            h = min(value, self.img_preview.height())
+            self.img_preview.setPixmap(QtGui.QPixmap(self.img_path).scaled(w, h, QtCore.Qt.KeepAspectRatio))
+        else:
+            w = min(self.img_size.width(), self.img_preview.width())
+            h = min(value, self.img_preview.height())
+            self.img_preview.setPixmap(QtGui.QPixmap(self.img_path).scaled(w, h))
+        self.img_size.setHeight(value)
+        self.unsaved_changes = self.img_size == self.saved_size
+
     def set_image(self, image_path: str):
         self.img_size = QtGui.QImage(image_path).size()
+        self.saved_size = self.img_size
+        self.original_aspect_ratio = self.img_size.width() / self.img_size.height()
+
+        self.width_spin_box.blockSignals(True)
+        self.width_spin_box.setValue(self.img_size.width())
+        self.width_spin_box.blockSignals(False)
+        self.height_spin_box.blockSignals(True)
+        self.height_spin_box.setValue(self.img_size.height())
+        self.height_spin_box.blockSignals(False)
+
         w = min(self.img_preview.width(), self.img_size.width())
         h = min(self.img_preview.height(), self.img_size.height())
         self.img_preview.setPixmap(QtGui.QPixmap(image_path).scaled(w, h, QtCore.Qt.KeepAspectRatio))
